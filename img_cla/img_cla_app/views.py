61f8070a1b9524f05e11e288b6644aa8d2b6b3fd
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-05-04 04:36:38 +0530

    prediction done

--- img_cla/img_cla_app/views.py
+++ img_cla/img_cla_app/views.py
@@ -4,32 +4,42 @@ from django.core.files.storage import FileSystemStorage
 from keras.preprocessing import image
 import numpy as np
 from keras.models import load_model
+import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Create your views here.
 
 
 def index(request):
     
     if request.method == "POST":
+        if request.session.has_key('uploaded_file'):
+            file_urls = request.session.get('uploaded_file')
+            os.remove(f"./{file_urls}")
+            del request.session['uploaded_file']
+            
         prediction = ""
         file = request.FILES['image']
         fs = FileSystemStorage()
         filename = fs.save(file.name, file)
-        uploaded_file_url = fs.url(filename)
+        request.session['uploaded_file'] = fs.url(filename)
+        file_urls = request.session.get('uploaded_file')
+     
         model = load_model('./saved_model/CNN_Cat_Dog_Model.h5')
         model.summary()
         model.weights
         model.optimizer
-        test_image = image.load_img(f"./{uploaded_file_url}", target_size=(64, 64))
+        test_image = image.load_img(f"./{file_urls}", target_size=(64, 64))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
         result = model.predict(test_image)
         if result[0][0] == 1:
-            prediction = 'dog'
+            prediction = 'DOG'
         else:
-            prediction = 'cat'
+            prediction = 'CAT'
             
         mydict = {
-            'img' :uploaded_file_url,
+            'img': file_urls,
             'pre': prediction,
         }
     
